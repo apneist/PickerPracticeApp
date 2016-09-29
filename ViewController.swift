333a2626f7e4633cc3a59e685565de02f96e177e
apneist <apneist@gmail.com>
2016-09-29 02:24:36 +0100

    made 2 first layouts work. need to fix mappings correctly
    innit geeza

--- ViewController.swift
+++ ViewController.swift
@@ -12,24 +12,40 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     
     
     
-    var myArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    var myArray = ["E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D"]
     
     var numbers: [String] = []
     
     
     @IBOutlet weak var PickerView: UIPickerView!
     
+    @IBOutlet weak var secondPicker: UIPickerView!
+    
+    @IBOutlet weak var thirdPicker: UIPickerView!
+    
+    @IBOutlet weak var fourthPicker: UIPickerView!
+    
     @IBOutlet weak var myNumberLabel: UILabel!
     
+    @IBOutlet weak var mySecondNumberLabel: UILabel!
     
+    @IBOutlet weak var PFaFGLabel: UILabel!
 
+    @IBOutlet weak var fristerLabel: UILabel!
 
 
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-
+        PickerView.dataSource = self
+        PickerView.delegate = self
+        secondPicker.dataSource = self
+        secondPicker.delegate = self
+        thirdPicker.dataSource = self
+        thirdPicker.delegate = self
+        fourthPicker.dataSource = self
+        fourthPicker.delegate = self
         
         var  alphabet: [Any] = []
         
@@ -78,11 +94,37 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     }
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        myNumberLabel.text = "\(numbers[row])"    }
+        
+        if pickerView == PickerView {
+        
+        myNumberLabel.text = "\(numbers[row])"
+        PFaFGLabel.text = "\(myArray[row])"
+        } else {
+        
+            if pickerView == secondPicker {
+                mySecondNumberLabel.text = "\(numbers[row])"
+                fristerLabel.text = "\(myArray[row])"
+                
+            }
+            
+        }
+        
+    }
 
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return myArray[row]
-    }
-    
-}
+        
+        if secondPicker == pickerView {
+           
+            return myArray[row]
+        
+        } else {
 
+            if PickerView == pickerView {
+            
+            return myArray[row]
+        
+        }
+            return ""
+}
+}
+}
