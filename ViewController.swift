56f6026538d44e99ff4afba78c80bff7c7302e98
apneist <apneist@gmail.com>
2016-09-30 00:13:03 +0100

    finished app - just auto-layout to fix
    all done

--- ViewController.swift
+++ ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     
     
     
-    var myArray = ["E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D"]
+    var myArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
     
     var numbers: [String] = []
     
@@ -40,10 +40,30 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     @IBOutlet weak var fristerLabel: UILabel!
 
     @IBOutlet weak var vestALabel: UILabel!
+    
+    var numbersPfaff = ["-19", "-18", "-17", "-16", "-15", "-14", "-13", "-12", "-11", "-10", "-9", "-8", "-7", "-6", "-5", "-4", "-3", "-2", "-1", "0", "1", "2", "3", "4", "5", "6"]
+    
+    var numbersGeneric = ["-4", "-3", "-2", "-1", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22"]
+    
+    var numbersFrister = ["-9", "-8", "-7", "-6", "-5", "-4", "-3", "-2", "-1", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16"]
+    
+    var numbersSinger = ["-5", "-4", "-3", "-2", "-1", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"]
+    
+    var numbersVesta = ["-23", "-22", "-21", "-20", "-19", "-18", "-17", "-16", "-15", "-14", "-13", "-12", "-11", "-10", "-9", "-8", "-7", "-6", "-5", "-4", "-3", "-2", "-1", "0", "1", "2"]
 
+    var newNumbersPfaff = [String]()
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    
         
+        numbersPfaff = numbersPfaff.reversed()
+        numbersFrister = numbersFrister.reversed()
+        numbersSinger = numbersSinger.reversed()
+        numbersVesta = numbersVesta.reversed()
         
       
         PickerView.dataSource = self
@@ -55,13 +75,36 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         fourthPicker.dataSource = self
         fourthPicker.delegate = self
         
+        
+        
+        PickerView.selectRow(6, inComponent: 0, animated: true)
+        secondPicker.selectRow(16, inComponent: 0, animated: true)
+        thirdPicker.selectRow(20, inComponent: 0, animated: true)
+        fourthPicker.selectRow(2, inComponent: 0, animated: true)
+        
         var  alphabet: [Any] = []
         
-
+        var ReverseNumbersPfaff = [String]()
+        
+        ReverseNumbersPfaff = numbersPfaff.reversed()
+        
+        print (ReverseNumbersPfaff)
+        
+        let ReverseNumbersFrister = numbersFrister.reversed()
+        
+        print (ReverseNumbersFrister)
         
         let Abc:String = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        
+        for a in Abc.characters.reversed() {
+            
+         newNumbersPfaff.append(String(a))
+            
+        }
+        
+        print ("AAAAAAAAAAAAAAAAAA")
+        print (newNumbersPfaff)
 
-        print ("herroowwwwww")
         print (Abc.characters)
         
         for i in Abc.characters {
@@ -74,16 +117,12 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         
         print (alphabet)
         
-
-        
         for number in 0...myArray.count {
             
             numbers.append(String(number))
         }
         
         print (numbers)
-        
-        
         
        
     }
@@ -103,28 +142,32 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         return myArray.count
     }
     
+    
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         
         if pickerView == PickerView {
         
-        myNumberLabel.text = "\(numbers[row])"
+        myNumberLabel.text = "\(numbersPfaff[row])"
         PFaFGLabel.text = "\(myArray[row])"
+       
+        
+        
         } else {
         
             if pickerView == secondPicker {
-                mySecondNumberLabel.text = "\(numbers[row])"
+                mySecondNumberLabel.text = "\(self.numbersFrister[row])"
                 fristerLabel.text = "\(myArray[row])"
                 
             } else {
                 
                 if pickerView == thirdPicker {
-                    myThirdNumberLabel.text = "\(numbers[row])"
+                    myThirdNumberLabel.text = "\(numbersSinger[row])"
                     SingeULabel.text = "\(myArray[row])"
                     
                 } else {
                     
                     if pickerView == fourthPicker {
-                        myFourthNumberLabel.text = "\(numbers[row])"
+                        myFourthNumberLabel.text = "\(numbersVesta[row])"
                         vestALabel.text = "\(myArray[row])"
                         
                     }
@@ -165,4 +208,12 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
             }
 }
 }
-}
+    
+    
+    
+    
+    }
+    
+    
+    
+
